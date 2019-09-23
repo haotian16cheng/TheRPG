92575933bd6f5cc9ff38804c6494654851a2fb6b
haotian16cheng <haotian16cheng@163.com>
2019-09-24 01:40:01 +0800

    【更新】更新策划案，战斗系统

--- Game/Main.py
+++ Game/Main.py
@@ -4,9 +4,9 @@ import sys
 pygame.init()
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
-
+isGaming = True
 #主要循环
-while True:
+while isGaming:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit(0)
